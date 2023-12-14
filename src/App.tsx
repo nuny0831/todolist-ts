75bd8646811ed6a3764199cd57536fd07c5e07b2
nuny0831 <yrch92@gmail.com>
2023-12-14 23:06:26 +0900

    초기세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
-import './App.css';
+import styled from 'styled-components';
 
 function App() {
-    return <div className="App"></div>;
+    return <>
+    <HomeBox>
+안녕하세요 잘되나요?
+    </HomeBox>
+    </>;
 }
 
 export default App;
+
+const HomeBox = styled.div`
+  max-width: 1200px;
+  min-width: 800px;
+  margin: 0 auto;
+  padding: 0 20px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  `;
