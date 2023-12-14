fac6bbd121771c03157e64999fabc3c53661fa8e
nuny0831 <yrch92@gmail.com>
2023-12-14 23:16:23 +0900

    UI 기초세팅 중

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,35 @@ import React from 'react';
 import styled from 'styled-components';
 
 function App() {
-    return <>
-    <HomeBox>
-안녕하세요 잘되나요?
-    </HomeBox>
-    </>;
+  return (
+    <>
+      <HomeBox>
+        <TitleBox>
+          <TitleInput placeholder="제목"></TitleInput>
+          <TitleInput placeholder="할 일"></TitleInput>
+          <button>추가</button>
+        </TitleBox>
+        <ListBox>
+          할일
+          <Card>
+            <div>제목</div>
+            <div>내용</div>
+            <button>삭제</button>
+            <button>완료</button>
+          </Card>
+        </ListBox>
+        <ListBox>
+          완료
+          <Card>
+            <div>제목</div>
+            <div>내용</div>
+            <button>삭제</button>
+            <button>취소</button>
+          </Card>
+        </ListBox>
+      </HomeBox>
+    </>
+  );
 }
 
 export default App;
@@ -20,4 +44,26 @@ const HomeBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  `;
+  flex-direction: column;
+`;
+
+const TitleInput = styled.input`
+  margin: 10px;
+  font-size: 20px;
+  padding-left: 10px;
+`;
+
+const TitleBox = styled.div`
+  margin: 20px;
+`;
+
+const ListBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Card = styled.div`
+  border: 2px solid blue;
+  width: 200px;
+  height: 200px;
+`;
